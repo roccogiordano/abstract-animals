a652f6dbae4ac2e74e318623613a9a2b1be7b5ca
roccogiordano <rocco.giordano573@gmail.com>
2024-12-12 17:31:41 +0100

    feat: added Animals methods

--- abstract-animals/src/Animals.java
+++ abstract-animals/src/Animals.java
@@ -1,3 +1,23 @@
-public class Animals {
+// Animals Abstract Class
+
+public abstract class Animals {
     
+
+    // Animals Methods
+
+    public static void sleep() {
+
+        System.out.println("Zzzz...");
+
+    }
+
+    public abstract void eat();
+
+    public abstract void doSound();
+
+    //
+
+
 }
+
+//
