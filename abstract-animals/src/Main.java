99ea3078fa8bc54a2626920f07c40db8f78a754b
roccogiordano <rocco.giordano573@gmail.com>
2024-12-12 18:07:40 +0100

    feat: added Main with class methods

---
+++ abstract-animals/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+
+
+    // Call to Interfaces Methods
+    
+    public static void makeItFly(FlyingAnimals flyingAnimal) {
+
+        flyingAnimal.fly();
+
+    }
+
+    public static void makeItSwim(SwimmingAnimals flyingAnimal) {
+
+        flyingAnimal.swim();
+
+    }
+
+    //
+
+
+    public static void main(String[] args) {
+
+
+        // Objects Instances
+        
+        Dog dog = new Dog();
+
+        Eagle eagle = new Eagle();
+
+        Dolphin dolphin = new Dolphin();
+        
+        Sparrow sparrow = new Sparrow();
+
+        //
+
+
+        // Objects Methods
+
+        dog.eat();
+        dog.doSound();
+
+        eagle.eat();
+        eagle.doSound();
+        makeItFly(eagle);
+
+        dolphin.eat();
+        dolphin.doSound();
+        makeItSwim(dolphin);
+
+        sparrow.eat();
+        sparrow.doSound();
+        makeItFly(sparrow);
+
+        Animals.sleep();
+
+        //
+        
+
+    }
+}
